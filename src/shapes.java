0f35fd34e703f9c6d159a76bcb96eb6a29c259a5
coditum <coditum@10.144.4.16>
2019-09-21 15:43:38 -0400

    hi

--- src/shapes.java
+++ src/shapes.java
@@ -3,27 +3,155 @@ public class shapes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for (int i = 0; i < 10; i++){
+		int n = 5;
+		System.out.println("triangle one");
+		for (int i = 0; i < n; i++){
+			for(int j = 0; j < i; j++){
+				 System.out.print("* ");
+			}
+			System.out.println(" ");
+		}
+		System.out.println("triangle two");
+		for (int i = 0; i < n; i++){
+			for(int j = 0; j < i; j++){
+				 System.out.print("* ");
+			}
+			System.out.println(" ");
+		}
+		System.out.println("triangle three");
+		for (int i = n; i > 0; i--){
 			for(int j = 0; j < i; j++){
 				 System.out.print("* ");
 			}
 			System.out.println(" ");
 		}
 		System.out.println();
-		for (int i = 10; i > 0; i--){
-			for(int j = 0; j < i; j++){
-				 System.out.print("* ");
-			}
-			System.out.println(" ");
-		}
-		System.out.println();
-		for (int i = 10; i < 0; i++){
+		System.out.println("= triangle");
+		for (int i = n; i > 0; i--){
 			for(int j = 0; j < i; j++){
 				 System.out.print(" ");
+			
+				
 			}
-			//for (int k = 10 )
-			System.out.println(" ");
+			for(int k = n; k > i; k--){
+				System.out.print(" *");
+			}
+			System.out.println();
+			
 		}
+		System.out.println("Diamond");
+		for (int i = n; i > 0; i--){
+			for(int j = 0; j < i; j++){
+				 System.out.print(" ");
+			
+				
+			}
+			for(int k = n; k > i; k--){
+				System.out.print(" *");
+			}
+			System.out.println();
+			
+		}
+		for (int i = n; i > 0; i--){
+			for(int j = n; j > i; j--){
+				 System.out.print(" ");
+			
+				
+			}
+			for(int k = 0; k < i; k++){
+				System.out.print(" *");
+			}
+			System.out.println();
+			
 	}
-
+		
+		System.out.println();
+		System.out.println("Hollow Diamond");
+		for (int i = n; i > 0; i--){
+			for(int j = 0; j < i; j++){
+				 System.out.print(" ");
+			
+				
+			}
+			for(int k = n; k > i; k--){
+				System.out.print("*");
+			
+			for(int l = 4; l > i; l--){
+				System.out.print(" ");
+			}
+			}
+			System.out.println();
+			
+		}
+		for (int i = n; i > 0; i--){
+			for(int j = n; j > i; j--){
+				 System.out.print(" ");
+			
+				
+			}
+			for(int k = 0; k < i; k++){
+				System.out.print("* ");
+			}
+			System.out.println();
+			
+	}
+		System.out.println("Hollow Diamond");
+		System.out.println("     *      ");
+		System.out.println("    * *  ");
+		System.out.println("   *   *");
+		System.out.println("  *     *");
+		System.out.println(" *       *");
+		System.out.println("  *     *");
+		System.out.println("   *   *");
+		System.out.println("    * *");
+		System.out.println("     *");
+		
+		System.out.println("Hollow Diamond");
+		for (int i = n; i > 0; i--){
+			for(int j = 0; j < i; j++){
+				 System.out.print("_");
+			
+				
+			}
+			System.out.println("*");
+			
+			
+		}
+		
+		for (int i = n; i > 0; i--){
+			for(int j = n; j > i; j--){
+				 System.out.print("_");
+			
+				
+			}
+			for(int k = 0; k < i; k++){
+				
+			}
+			System.out.println("*");
+			
+	}
+		for (int i = n; i > 0; i--){
+			for(int j = n; j > i; j--){
+				 System.out.print("_");
+			
+				
+			}
+			for(int k = 0; k < i; k++){
+				
+			}
+			System.out.println("*");
+			
+	}
+		for (int i = n; i > 0; i--){
+			for(int j = 0; j < i; j++){
+				 System.out.print("_");
+			
+				
+			}
+			System.out.println("*");
+			
+			
+		}
+		
+	}
 }
